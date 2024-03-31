22feb1a0e816f57389b748710d74801f74b8f591
Kanin Sukittivarapunt <newty40@gmail.com>
2024-03-31 16:46:19 +0700

    complete new project structure and database connection

--- database/connection.go
+++ database/connection.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,40 +10,28 @@ import (
 var Db *gorm.DB
 var err error
 
+func InitDB() *gorm.DB {
+	// Get database credentials from environment variables
+	username := os.Getenv("MYSQL_USERNAME")
+	password := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbName := os.Getenv("MYSQL_DATABASE")
 
-// func InitDB() *gorm.DB{
-//     // Get database credentials from environment variables
-//     username := os.Getenv("MYSQL_USERNAME")
-//     password := os.Getenv("MYSQL_PASSWORD")
-//     host := os.Getenv("MYSQL_HOST")
-//     port := os.Getenv("MYSQL_PORT")
-//     dbName := os.Getenv("MYSQL_DATABASE")
+	// Construct the DSN using environment variables
+	dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-//     // Construct the DSN using environment variables
-//     dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-
-//     // Open a connection to the MySQL database
-//     Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//     if err != nil {
-//         panic("failed to connect database: " + err.Error())
-//     }
-
-//     // Migrate the schema
-//     err = Db.AutoMigrate(&UserAuth{})
-//     if err != nil {
-//         panic("failed to migrate schema: " + err.Error())
-//     }
-	
-// 	return Db
-// }
-func InitDB() {
-	dsn := os.Getenv("MYSQL_DSN")
+	// Open a connection to the MySQL database
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("failed to connect database: " + err.Error())
 	}
 
 	// Migrate the schema
-	Db.AutoMigrate(&UserAuth{},&User{},&Schedule{},&Wishlist{},&Location{})
-}
+	err = Db.AutoMigrate(&UserAuth{}, &User{}, &Schedule{}, &Wishlist{}, &Location{})
+	if err != nil {
+		panic("failed to migrate schema: " + err.Error())
+	}
 
+	return Db
+}
--- database/model.go
+++ database/model.go
@@ -1,9 +1,10 @@
 package database
 
 import (
-	"gorm.io/gorm"
 	"time"
+
 	"gorm.io/datatypes"
+	"gorm.io/gorm"
 )
 
 type UserAuth struct {
@@ -13,61 +14,53 @@ type UserAuth struct {
 	TelNo    string
 }
 
-type User struct{
+type User struct {
 	gorm.Model
-	Title string
-	Name string
-	Birthdate time.Time
-	Region string
-	TelNo  string
-	ProfilePicture string   
-	Friends []User `gorm:"many2many:UserProfile"`
-	Schedules []Schedule `gorm:"many2many:Invitee"`
+	Title          string
+	Name           string
+	Birthdate      time.Time
+	Region         string
+	TelNo          string
+	ProfilePicture string
+	Friends        []User     `gorm:"many2many:UserProfile"`
+	Schedules      []Schedule `gorm:"many2many:Invitee"`
 }
 
-type Schedule struct{
+type Schedule struct {
 	gorm.Model
-	Name string
-	StartTime time.Time
-	EndTime time.Time
-	StartDate time.Time
-	EndDate time.Time
-	Type string 
-	IsAllDay bool
-	Tag string 
-	Note string
-	QrCode string
+	Name           string
+	StartTime      time.Time
+	EndTime        time.Time
+	StartDate      time.Time
+	EndDate        time.Time
+	Type           string
+	IsAllDay       bool
+	Tag            string
+	Note           string
+	QrCode         string
 	IsNotification bool
-	ColorCode string
-	Repeat string
-	Weekly datatypes.JSON
-	Monthly  datatypes.JSON
-	LocationID uint
-	ParentID *uint 
-	Children []Schedule `gorm:"foreignKey:ParentID"`
-	Users []User `gorm:"many2many:Invitee"`
-	
+	ColorCode      string
+	Repeat         string
+	Weekly         datatypes.JSON
+	Monthly        datatypes.JSON
+	LocationID     uint
+	ParentID       *uint
+	Children       []Schedule `gorm:"foreignKey:ParentID"`
+	Users          []User     `gorm:"many2many:Invitee"`
 }
 
-type Location struct{
-	gorm.Model 
-	Latitude string 
-	Longtitude string 
-	Name string
-	Schedules []Schedule
+type Location struct {
+	gorm.Model
+	Latitude   string
+	Longtitude string
+	Name       string
+	Schedules  []Schedule
 }
 
-type Wishlist struct{
-	DateTime time.Time
-	ScheduleId uint `gorm:"primaryKey"`
-	Schedule Schedule `gorm:"foreignKey:ScheduleId"`
-	LocationId uint `gorm:"primaryKey"`
-	Location Location `gorm:"foreignKey:LocationId"`
+type Wishlist struct {
+	DateTime   time.Time
+	ScheduleId uint     `gorm:"primaryKey"`
+	Schedule   Schedule `gorm:"foreignKey:ScheduleId"`
+	LocationId uint     `gorm:"primaryKey"`
+	Location   Location `gorm:"foreignKey:LocationId"`
 }
-
-
-
-
-
-
-
