5588d32598d26c2745e700287003788f9eadfffe
guykrittin <krittin.siangprasertkij@outlook.com>
2024-04-19 02:40:30 +0700

    initialize sync by friends repo

--- packages/schedulesync/repo/scheduleSyncRepo.go
+++ packages/schedulesync/repo/scheduleSyncRepo.go
@@ -10,6 +10,7 @@ import (
 
 type ScheduleSyncRepoInterface interface {
     GetFriendsSchedules(uid string, startDate time.Time, endDate time.Time) (map[string][]database.Schedule, error)
+	GetSpecificSchedules(uid string, friendUIDs []string, startDate time.Time, endDate time.Time) (map[string][]database.Schedule, error)
 }
 
 type scheduleSyncRepo struct {
@@ -61,4 +62,41 @@ func (r *scheduleSyncRepo) GetFriendsSchedules(uid string, startDate time.Time, 
     }
 
     return friendSchedules, nil
+}
+
+func (r *scheduleSyncRepo) GetSpecificSchedules(uid string, friendUIDs []string, startDate time.Time, endDate time.Time) (map[string][]database.Schedule, error) {
+    specificSchedules := make(map[string][]database.Schedule)
+
+	// add user uid to the frienduid
+	friendUIDs = append(friendUIDs, uid)
+    // Loop through the given friend's UIDs
+    for _, friendUID := range friendUIDs {
+        var friend database.User
+
+        // Get the friend with the given UID
+        err := r.dbConn.First(&friend, "uid = ?", friendUID).Error
+        if err != nil {
+            log.Printf("Error getting friend with uid %s: %v", friendUID, err)
+            return nil, err
+        }
+
+        // Get the active schedules of the friend that are within the given time and date range
+        var schedules []database.Schedule
+        db := r.dbConn.Joins("LEFT JOIN invitations on invitations.schedule_id = schedules.id").
+            Where("(schedules.status = 'Active' and invitations.status = 'Active' and invitations.receiver_uid =?) or (schedules.status = 'Active' and schedules.host_id=?)", friend.Uid, friend.Uid).
+            Where("start_date >= ? AND end_date <= ?", startDate, endDate).
+            Preload("Invitations", "status='Active'").
+            Preload("Invitations.Receiver").
+            Find(&schedules)
+
+        if db.Error != nil {
+            log.Printf("Error executing schedule query: %v", db.Error)
+            return nil, db.Error
+        }
+
+        // Add the schedules to the map
+        specificSchedules[friend.Uid] = schedules
+    }
+
+    return specificSchedules, nil
 }
